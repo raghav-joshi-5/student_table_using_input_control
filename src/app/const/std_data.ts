7b5469942544a345156237aeba9dcbeff7618644
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-12-18 00:23:44 +0530

    array_update

--- src/app/const/std_data.ts
+++ src/app/const/std_data.ts
@@ -1,25 +1,3 @@
 import { Istd } from '../model/std_data';
 
-export const student: Array<Istd> = [
-  {
-    id: '1',
-    name: 'John',
-    lname: 'Doe',
-    contact: '123-456-7890',
-    email: 'johndoe@example.com',
-    address: '123 Maple Street, Springfield',
-    grade: 'A',
-    age: 20,
-  },
-
-  {
-    id: '2',
-    name: 'Jane',
-    lname: 'Smith',
-    contact: '987-654-3210',
-    email: 'janesmith@example.com',
-    address: '456 Oak Avenue, Riverdale',
-    grade: 'B',
-    age: 22,
-  },
-];
+export const student: Array<Istd> = [];
